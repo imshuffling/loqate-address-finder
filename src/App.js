1f56c6e6cf9eeb771020aa22052e0780371eafd5
David Riches <david.riches@atomsupplies.com>
2023-02-02 22:36:02 +0000

    adding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import AddressSearch from 'react-loqate';
+import 'react-loqate/dist/react-loqate.cjs.development.css';
 
-function App() {
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
+const App = (props) => {
+  const [addressLine, setAddressLine] = useState('');
+  const [addressLine2, setAddressLine2] = useState('');
+  const [city, setCity] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+
+  const addressSelected = (address) => {
+    console.log(address);
+    setAddressLine(address.Line1);
+    setAddressLine2(address.Line2);
+    setCity(address.City);
+    setPostalCode(address.PostalCode);
+  };
+
+  const AddressSearchInput = (props) => {
+    return (
+      <TextField
+        id='outlined-basic'
+        label='Type your address or post code...'
+        autocomplete='chrome-off'
+        variant='outlined'
+        fullWidth
+        {...props}
+      />
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth='sm'>
+      <Typography variant='h4' gutterBottom={true}>
+        Loqate Address Search
+      </Typography>
+      <AddressSearch
+        locale='en-GB'
+        apiKey='XT96-FX22-WY91-NJ79'
+        onSelect={(address) => addressSelected(address)}
+        countries={['GB', 'AUS', 'NZL', 'IRL']}
+        components={{ Input: AddressSearchInput }}
+        className='address-search-box'
+        debounce={100}
+      />
+      <Box
+        component='form'
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete='off'
+      >
+        <TextField
+          id='standard-basic'
+          label='Address Line 1'
+          variant='standard'
+          value={addressLine}
+        />
+        <TextField
+          id='standard-basic'
+          label='Address Line 2'
+          variant='standard'
+          value={addressLine2}
+        />
+        <TextField
+          id='standard-basic'
+          label='City'
+          variant='standard'
+          value={city}
+        />
+        <TextField
+          id='standard-basic'
+          label='Post Code'
+          variant='standard'
+          value={postalCode}
+        />
+      </Box>
+    </Container>
   );
-}
+};
 
 export default App;
